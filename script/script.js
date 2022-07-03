21a453acc72a6d60697fadacb7ae107ad4cb6982
rijvanamd26 <103577943+rijvanamd26@users.noreply.github.com>
2022-07-03 19:44:40 +0530

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,22 @@
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:10,
+    responsiveClass:true,
+    autoplay:true,
+    autoPlaySpeed:2000,
+    responsive:{
+        0:{
+            items:1,
+            nav:true
+        },
+        600:{
+            items:3,
+            nav:false
+        },
+        1000:{
+            items:4,
+            nav:true,
+            loop:true
+        }
+    }
+})
